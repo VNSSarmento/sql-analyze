dd5354497e0a52c0117fe272844807ad830dcdfe
Vinicius Sarmento <sarmento.ramos11@gmail.com>
2026-08-05 12:19:20 -0300

    estabelecendo arquitetura de arquivos de configuração com a separação de um arquivo de configuração para o postgres e outro para o redis

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -2,13 +2,23 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 	"sql-analyze/internal/config"
+
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	bd := config.NewConn()
+	route := gin.Default()
 
 	if bd != nil {
 		fmt.Println("Banco conectado com sucesso")
 	}
+
+	route.GET("/health", func(ctx *gin.Context) {
+		ctx.JSON(http.StatusOK, gin.H{"message": "tá batendo"})
+	})
+
+	route.Run()
 }
