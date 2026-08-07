18f36c079d02fd9a5534201c52c392e21eec2793
Vinicius Sarmento <sarmento.ramos11@gmail.com>
2026-08-07 13:57:30 -0300

    inserindo interface do repositorio, e struct da entidade Query

--- internal/domain/query.go
+++ internal/domain/query.go
@@ -1,1 +1,15 @@
 package domain
+
+import "time"
+
+type Query struct {
+	QueryId         string
+	DbUser          string
+	NormalizedQuery string
+	ExecutionsCount int64
+	MeanTimeMs      float64
+	M2              float64
+	LastExecutionAt time.Time
+	LastAnomalyAt   time.Time
+	CreatedAt       time.Time
+}
--- internal/domain/repository.go
+++ internal/domain/repository.go
@@ -1,1 +1,7 @@
 package domain
+
+type QueryRepository interface {
+	GetByID(queryID, dbUser string) (*Query, error)
+	Save(*Query) error
+	GetQueryLimit(limit int) []*Query
+}
