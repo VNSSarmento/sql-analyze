1cf63bf645f8c554f572c8fd40b4da54d945bd45
Vinicius Sarmento <sarmento.ramos11@gmail.com>
2026-08-07 14:56:54 -0300

    construtor do caso de uso criado

--- internal/usecase/analyze_queries.go
+++ internal/usecase/analyze_queries.go
@@ -1,1 +1,15 @@
 package usecase
+
+import "sql-analyze/internal/domain"
+
+type AnalyzeQueryUseCase struct {
+	repository domain.QueryRepository
+	pubAlert   domain.AlertPublisher
+}
+
+func NewAnalyzeQueryUseCase(repository domain.QueryRepository, publishe domain.AlertPublisher) *AnalyzeQueryUseCase {
+	return &AnalyzeQueryUseCase{
+		repository: repository,
+		pubAlert:   publishe,
+	}
+}
