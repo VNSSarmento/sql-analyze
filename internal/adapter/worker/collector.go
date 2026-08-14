93b2604160a04db7be6941b46b58b5ec6a576832
Vinicius Sarmento <sarmento.ramos11@gmail.com>
2026-08-14 13:28:07 -0300

    work coletor criado

--- internal/adapter/worker/collector.go
+++ internal/adapter/worker/collector.go
@@ -2,6 +2,7 @@ package worker
 
 import (
 	"context"
+	"fmt"
 	"log"
 	"sql-analyze/internal/usecase"
 	"time"
@@ -21,19 +22,89 @@ type Collector struct {
 	snapshots      map[string]statSnapshot
 }
 
+func NewCollector(pool *pgxpool.Pool, analyzeUseCase *usecase.AnalyzeQueryUseCase, interval time.Duration) *Collector {
+	return &Collector{
+		pool:           pool,
+		analyzeUseCase: analyzeUseCase,
+		interval:       interval,
+		snapshots:      make(map[string]statSnapshot),
+	}
+}
+
 func (c *Collector) collectOnce(ctx context.Context) {
 
+	sql := "SELECT pss.queryid::text, r.rolname AS db_user, pss.query AS normalized_query, pss.calls, pss.total_exec_time AS total_exec_time_ms FROM pg_stat_statements pss JOIN pg_roles r ON pss.userid = r.oid WHERE pss.calls > 0;"
+
+	rows, err := c.pool.Query(ctx, sql)
+
+	if err != nil {
+		log.Printf("erro ao executar query no pg_stat_statements: %v", err)
+		return
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+
+		var queryID, dbUser, normalizedQuery string
+		var currentCalls int64
+		var currentTotalTime float64
+
+		err := rows.Scan(
+			&queryID, &dbUser,
+			&normalizedQuery,
+			&currentCalls,
+			&currentTotalTime)
+
+		if err != nil {
+			log.Printf("erro no scan: %v", err)
+			continue
+		}
+
+		key := fmt.Sprintf("%s:%s", queryID, dbUser)
+
+		prevSnapshot, exist := c.snapshots[key]
+
+		if !exist {
+			c.snapshots[key] = statSnapshot{
+				calls:           currentCalls,
+				totalExecTimeMs: currentTotalTime,
+			}
+			continue
+		}
+
+		callDelta := currentCalls - prevSnapshot.calls
+		timeDelta := currentTotalTime - prevSnapshot.totalExecTimeMs
+
+		if callDelta <= 0 {
+			c.snapshots[key] = statSnapshot{
+				calls:           currentCalls,
+				totalExecTimeMs: currentTotalTime,
+			}
+			continue
+		}
+
+		avgTimeMs := timeDelta / float64(callDelta)
+
+		err = c.analyzeUseCase.Execute(ctx, queryID, dbUser, normalizedQuery, avgTimeMs)
+		if err != nil {
+			log.Printf("work error: %v", err)
+		}
+
+		c.snapshots[key] = statSnapshot{calls: callDelta, totalExecTimeMs: timeDelta}
+
+	}
 }
 
 func (c *Collector) Start(ctx context.Context) {
 	ticker := time.NewTicker(c.interval)
+	defer ticker.Stop()
 
 	for {
 		select {
+		case <-ctx.Done():
+			return
 		case <-ticker.C:
 			c.collectOnce(ctx)
-		case <-ctx.Done():
-			log.Println("Tempo Finalizado")
 		}
 	}
 }
